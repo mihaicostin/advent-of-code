e4b76d7ac71014a50cd51a72ad5346607fb08d2f
Mihai Costin <mihai.costin@gmail.com>
2019-12-02 17:17:35 +0200

    day1.1

--- aoc-2019-rust/src/bin/day1.rs
+++ aoc-2019-rust/src/bin/day1.rs
@@ -10,11 +10,19 @@ fn main() {
     println!("Result:\n{:?}", result);
 }
 
-
 fn lines_from_file(filename: impl AsRef<Path>) -> i32 {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
     buf.lines()
-        .map(|l| l.expect("Could not parse line").parse::<i32>().expect("not a number?")/3 - 2)
+        .map(|l| extraFuel(l.expect("Could not parse line").parse::<i32>().expect("not a number?")/3 - 2))
         .sum()
+}
+
+
+fn extraFuel(a: i32) -> i32  {
+    if a <= 0 {
+        return 0;
+    } else {
+       return a + ffun(a / 3 - 2)
+    }
 }
