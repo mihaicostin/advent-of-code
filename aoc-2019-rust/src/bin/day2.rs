bdeef250f66d1b875883415189071135a97d3e85
Mihai Costin <mihai.costin@gmail.com>
2019-12-02 23:02:42 +0200

    day2.2

--- aoc-2019-rust/src/bin/day2.rs
+++ aoc-2019-rust/src/bin/day2.rs
@@ -8,12 +8,21 @@ use std::{
 fn main() {
     let mut result = read_comma_int_array("src/bin/day2.txt");
 
-    result[1] = 12;
-    result[2] = 2;
+    let mut done = false;
+    let mut a = 0;
+    let mut b = 0;
 
-    let r = process_array(result);
-
-    println!("Result:\n{:?}", r[0]);
+    for a in 0..100 {
+        for b in  0..100 {
+            let mut input = result.to_vec();
+            input[1] = a;
+            input[2] = b;
+            let r = process_array(input);
+            if r[0] == 19690720 {
+                println!("Result a:\n{:?}", 100* a + b);
+            }
+        }
+    }
 }
 
 fn read_comma_int_array(filename: impl AsRef<Path>) -> Vec<i32> {
@@ -30,35 +39,36 @@ fn read_comma_int_array(filename: impl AsRef<Path>) -> Vec<i32> {
 }
 
 
-
-fn process_array(mut input: Vec<i32>) -> Vec<i32> {
+fn process_array(input: Vec<i32>) -> Vec<i32> {
 
     let mut i = 0 ;
 
-    while i < input.len() {
+    let mut result = input.to_vec();
 
-        let opCode = input[i];
+    while i < result.len() {
+
+        let op_code = result[i];
 
 
-        if opCode == 99 {
+        if op_code == 99 {
             break;
         }
-        else if opCode == 1 {
+        else if op_code == 1 {
             //add
-            let index3 = input[i + 3] as usize;
+            let index3 = result[i + 3] as usize;
 
-            input[index3] = input[input[i + 1] as usize] + input[input[i + 2] as usize]
+            result[index3] = result[result[i + 1] as usize] + result[result[i + 2] as usize]
 
         }
-        else if opCode == 2 {
+        else if op_code == 2 {
             //multiply]
-            let index3 = input[i + 3] as usize;
-            input[index3] = input[input[i+1] as usize] * input[input[i+2] as usize]
+            let index3 = result[i + 3] as usize;
+            result[index3] = result[result[i+1] as usize] * result[result[i+2] as usize]
         }
 
         i = i + 4
     }
 
 
-    return input;
+    return result;
 }
