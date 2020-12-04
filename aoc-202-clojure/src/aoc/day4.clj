71cc75d393f8fe8f4db299dae2f2a79f089b4df1
Mihai Costin <mihai.costin@gmail.com>
2020-12-04 19:58:52 +0200

    2020: day4.2

--- aoc-202-clojure/src/aoc/day4.clj
+++ aoc-202-clojure/src/aoc/day4.clj
@@ -30,7 +30,7 @@
     (let [[line & rest] lines]
       (if (str/blank? line)
         (collect rest {} (conj result current))
-        (collect rest (merge current (toMap line) ) result))
+        (collect rest (merge current (toMap line)) result))
       )
     )
   )
@@ -47,14 +47,47 @@
 
 (defn isValid
   [passportMap]
+
   (and
+
+    ;byr (Birth Year) - four digits; at least 1920 and at most 2002.
     (contains? passportMap "byr")
+    (let [y (edn/read-string (get passportMap "byr"))]
+      (and (not (nil? y)) (>= y 1920) (<= y 2002)))
+
+    ;iyr (Issue Year) - four digits; at least 2010 and at most 2020.
     (contains? passportMap "iyr")
+    (let [y (edn/read-string (get passportMap "iyr"))]
+      (and (not (nil? y)) (>= y 2010) (<= y 2020)))
+
+    ;eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
     (contains? passportMap "eyr")
+    (let [y (edn/read-string (get passportMap "eyr"))]
+      (and (not (nil? y)) (>= y 2020) (<= y 2030)))
+
+    ;hgt (Height) - a number followed by either cm or in:
+    ;If cm, the number must be at least 150 and at most 193.
+    ;If in, the number must be at least 59 and at most 76.
     (contains? passportMap "hgt")
+    (let [[_ val units] (re-matches #"([0-9]+)(cm|in)" (get passportMap "hgt"))]
+      (cond
+        (= units "cm") (let [y (edn/read-string val)] (and (not (nil? y)) (>= y 150) (<= y 193)))
+        (= units "in") (let [y (edn/read-string val)] (and (not (nil? y)) (>= y 59) (<= y 76)))
+        :else false
+        )
+      )
+
+    ;hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
     (contains? passportMap "hcl")
+    (let [[_ val] (re-matches #"#([0-9a-f]{6})" (get passportMap "hcl"))] (not (nil? val)))
+
+    ;ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
     (contains? passportMap "ecl")
+    (let [[_ val] (re-matches #"(amb|blu|brn|gry|grn|hzl|oth)" (get passportMap "ecl"))] (not (nil? val)))
+
+    ;pid (Passport ID) - a nine-digit number, including leading zeroes.
     (contains? passportMap "pid")
+    (let [[_ val] (re-matches #"([0-9]{9})" (get passportMap "pid"))] (not (nil? val)))
     ))
 
 
@@ -65,14 +98,11 @@
   (def lines (u/readLines inputFile))
   (def processedLines (collect lines {} []))
 
-  ;(doseq [line processedLines]
-  ;  (println "Line: " line))
-
   (print "valid:"
-    (count
-      (filter isValid  processedLines)
-      )
-  )
+         (count
+           (filter isValid processedLines)
+           )
+         )
 
   )
 
