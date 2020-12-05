0bf516bcc44d634a3778ba1cd8801ed4c67db7b7
Mihai Costin <mihai.costin@gmail.com>
2020-12-05 11:59:34 +0200

    2020: day5.1

---
+++ aoc-202-clojure/src/aoc/day5.clj
@@ -0,0 +1,59 @@
+(ns aoc.day5
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]
+            [aoc.utilitybelt :as u]
+            ))
+
+(def inputFile "src/aoc/day5.txt")
+
+
+(defn decode
+  [value min max lk uk]
+
+  (def middle (/ (+ min (- max 1)) 2))
+
+  (let [[head & tail] value]
+    (if (empty? tail)
+      (cond
+        (= head lk) min
+        (= head uk) max
+        )
+      (if (= head \F)
+        (decode tail min middle lk uk)
+        (decode tail (+ middle 1) max lk uk)
+        ))
+    )
+
+  )
+
+
+(defn toBoardingPass
+  [lineStr]
+
+  (def row (decode (seq (subs lineStr 0 7)) 0 127 \F \B) )
+  (def col (decode (seq (subs lineStr 7 10)) 0 7 \L \R) )
+  (def id (+ (* row 8) col))
+
+  {:row row, :col col, :id id}
+
+  )
+
+
+
+
+(defn -main
+  "Day 5: Binary Boarding"
+  [& args]
+
+  (def lines (u/readLines inputFile))
+
+  (println
+    (reduce #(max %1 (get %2 :id))  0
+    (map toBoardingPass lines)
+    )
+    )
+
+  )
+
+
