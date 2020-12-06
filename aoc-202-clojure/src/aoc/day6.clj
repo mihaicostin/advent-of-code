f1706464ef87e2153f1b8df705541d8c8974f439
Mihai Costin <mihai.costin@gmail.com>
2020-12-06 08:01:15 +0200

    2020: day6.2

--- aoc-202-clojure/src/aoc/day6.clj
+++ aoc-202-clojure/src/aoc/day6.clj
@@ -2,22 +2,37 @@
   (:gen-class)
   (:require [clojure.string :as str]
             [clojure.edn :as edn]
+            [clojure.set :as s]
             [aoc.utilitybelt :as u]
             ))
 
 (def inputFile "src/aoc/day6.txt")
 
+;part 1
+;(defn collect
+;  [lines current result]
+;
+;  (if (empty? lines)
+;    (conj result current)
+;      (println "line, rest: " line rest)
+;      (if (str/blank? line)
+;        (collect rest '() (conj result current))
+;        (collect rest (distinct (concat current (seq line))) result))
+;      )
+;    )
+;  )
 
+;part 2
 (defn collect
   [lines current result]
 
   (if (empty? lines)
     (conj result current)
     (let [[line & rest] lines]
-      (println "line, rest: " line rest)
       (if (str/blank? line)
         (collect rest '() (conj result current))
-        (collect rest (distinct (concat current (seq line))) result))
+        (collect rest (conj current (set (seq line))) result)
+        )
       )
     )
   )
@@ -26,13 +41,13 @@
 
 
 (defn -main
-  "Day 6: Binary Boarding"
+  "Day 6: Custom Customs "
   [& args]
 
   (def lines (u/readLines inputFile))
 
-  (def lists (collect lines '() '()))
+  (def lists (collect lines '() []))
 
-  (print (reduce + (map count lists )))
+  (print (reduce + (map count (map #(reduce s/intersection %) lists))))
 
   )
