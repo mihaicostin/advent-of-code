244cf8db63c8c3668ed014afa5880c8e1f32dd18
Mihai Costin <mihai.costin@gmail.com>
2020-12-19 09:01:59 +0200

    2020: day17.2

--- aoc-202-clojure/src/aoc/day17.clj
+++ aoc-202-clojure/src/aoc/day17.clj
@@ -9,21 +9,21 @@
 (def inputFile "src/aoc/day17.txt")
 
 (defn parse
-  [line y z]
+  [line y z w]
   (map (fn [ch x] (case ch
-                    \. {:x x :y y :z z :on false}
-                    \# {:x x :y y :z z :on true}
+                    \. {:x x :y y :z z :w w :on false}
+                    \# {:x x :y y :z z :w w :on true}
                     )) (seq line) (range (count line)))
   )
 
 (defn getAt
-  [space x y z]
+  [space x y z w]
 
   (loop [remainingSpace space]
     (if (empty? remainingSpace)
-      {:x x :y y :z z :on false}
+      {:x x :y y :z z :w w :on false}
       (let [[head & tail] remainingSpace]
-        (if (and (= (:x head) x) (= (:y head) y) (= (:z head) z))
+        (if (and (= (:x head) x) (= (:y head) y) (= (:z head) z) (= (:w head) w))
           head
           (recur tail)
           )
@@ -39,15 +39,17 @@
   (let [sx (:x position)
         sy (:y position)
         sz (:z position)
+        sw (:w position)
         ]
     (filter some?
-            (for [z (vector (dec sz) sz (inc sz))
+            (for [w (vector (dec sw) sw (inc sw))
+                  z (vector (dec sz) sz (inc sz))
                   y (vector (dec sy) sy (inc sy))
                   x (vector (dec sx) sx (inc sx))
                   ]
-              (if (and (= x sx) (= y sy) (= z sz))
+              (if (and (= x sx) (= y sy) (= z sz) (= w sw))
                 nil
-                (getAt space x y z)
+                (getAt space x y z w)
                 )
               )
             )
@@ -56,7 +58,11 @@
 
 (defn expand
   [space]
-  (distinct (mapcat (fn [position] (conj (getNeighbours space position) position)) space))
+  (distinct (mapcat (fn [position] (if (true? (:on position))
+                                     (conj (getNeighbours space position) position)
+                                     (list position)
+                                     )
+                      ) space))
   )
 
 
@@ -83,7 +89,7 @@
 (defn nextState
   [space]
   (let [ex (expand space)]
-    (map (fn [position] (nextValue ex position)) ex)
+    (map (fn [position] (nextValue space position)) ex)
     )
   )
 
@@ -113,8 +119,9 @@
   [& args]
 
   (def lines (u/readLines inputFile))
+  (def spaceSize (count lines))
 
-  (def startingSpace (mapcat (fn [line idx] (parse line idx 0)) lines (range (count lines))))
+  (def startingSpace (mapcat (fn [line idx] (parse line idx 0 0)) lines (range (count lines))))
 
   (println
     (loop [space startingSpace
