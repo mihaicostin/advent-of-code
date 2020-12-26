1f6b06e7854cfccee22cc34b25725aa472e0c546
Mihai Costin <mihai.costin@gmail.com>
2020-12-26 13:53:58 +0200

    2020: day20... ?

--- aoc-202-clojure/src/aoc/day20.clj
+++ aoc-202-clojure/src/aoc/day20.clj
@@ -2,25 +2,27 @@
   (:require [aoc.utilitybelt :as u]
             [clojure.edn :as edn]
             [clojure.string :as str]
+            [clojure.math.numeric-tower :as math]
             ))
 
 (def inputFile "src/aoc/day20.txt")
 
 (defn parseTile
-  [lines]
+  [lines tileSize]
   (let [[_ id] (re-matches #"Tile (\d+):" (nth lines 0))
         tileString (str/join (drop 1 lines))]
-    {:uid (edn/read-string id), :size 10, :tileArray (seq (str/trim tileString))}))
+    {:uid (edn/read-string id), :size tileSize, :tileArray (seq (str/trim tileString))}))
 
 (defn parseInput
-  [lines]
+  [lines tileSize]
   (loop [processingLines lines
          result []]
     (if (= 0 (count processingLines))
       result
-      (let [tileArray (take 12 processingLines)
-            remaining (drop 12 processingLines)
-            tile (parseTile tileArray)
+      (let [nrLines (+ 2 tileSize)
+            tileArray (take nrLines processingLines)
+            remaining (drop nrLines processingLines)
+            tile (parseTile tileArray tileSize)
             ]
         (recur remaining (conj result tile))))))
 
@@ -28,10 +30,12 @@
 (defn getValueAt
   [tile x y]
   (let [size (:size tile)
-        tileArray (:tileArray tile)
-        ]
+        tileArray (:tileArray tile)]
     (nth tileArray (+ x (* y size)))))
 
+(defn getFromXY
+  [array w x y]
+  (nth array (+ x (* y w))))
 
 (defn transformFlipY
   [tile]
@@ -147,19 +151,164 @@
                   (let [matches (for [edge ["N" "E" "S" "W"]] (findMatch tile tiles edge))
                         matchedEdges (count (filter some? matches))]
                     (if (= 2 matchedEdges)
-                      tile
+                      (list tile (str/join (filter some? (map #(if (some? %1) %2 nil) matches ["N" "E" "S" "W"]))))
                       nil)))))
 
+(defn multiplyCorners
+  [inputTiles tileSize]
+
+  (let [tilesArray (parseInput inputTiles tileSize)
+        corners (map first (findCorners tilesArray))]
+    (reduce #(* %1 (:uid %2)) 1 corners)))
+
+(defn findFirstCorner
+  [tiles]
+  (first (first (filter #(= "ES" (last %)) (findCorners tiles)))))
+
+
+(defn getByUid
+  [tileArray uid]
+  (filter #(= (:uid %1) uid) tileArray))
+
+(defn dropByUid
+  [tileArray uid]
+  (filter #(not= (:uid %1) uid) tileArray))
+
+
+(defn buildMap
+  [tiles]
+  (let [mapSize (int (Math/sqrt (count tiles)))
+        indexes (for [y (range mapSize) x (range mapSize)] [x y])]
+    (loop [[[x y] & remainingIndexes] indexes
+           searchTiles tiles
+           foundTiles []]
+      (if (= nil x)
+        foundTiles
+        (if (and (= x 0) (= y 0))
+          (let [corner (findFirstCorner tiles)]
+            ;(println "found tile (corner) :" (:uid corner))
+            (recur remainingIndexes (dropByUid searchTiles (:uid corner)) (conj foundTiles corner)))
+          (if (= x 0)
+            (let [matchTargetTile (getValueAt {:tileArray foundTiles :size mapSize} 0 (- y 1))
+                  foundTile (findMatch matchTargetTile searchTiles "S")
+                  remainingSearchTiles (dropByUid searchTiles (:uid foundTile))
+                  ]
+              ;(println "found tile (first on row " y ") :" (:uid foundTile))
+              (recur remainingIndexes remainingSearchTiles (conj foundTiles foundTile)))
+            (let [matchTargetTile (last foundTiles)
+                  foundTile (findMatch matchTargetTile searchTiles "E")
+                  remainingSearchTiles (dropByUid searchTiles (:uid foundTile))
+                  ]
+              ;(println "found tile on row " y " :" (:uid foundTile))
+              (recur remainingIndexes remainingSearchTiles (conj foundTiles foundTile)))))))))
+
+
+(defn trimBorders
+  [imageMap]
+
+  (let [tileSize (:size (first imageMap))
+        borderlessTileSize (- tileSize 2)
+        mapSize (int (Math/sqrt (count imageMap)))
+        mapW (* mapSize borderlessTileSize)
+        tileArray (for [y (range mapW)
+                        x (range mapW)]
+                    (let [tx (int (math/floor (/ x borderlessTileSize)))
+                          ty (int (math/floor (/ y borderlessTileSize)))
+                          inTileX (rem x borderlessTileSize)
+                          inTileY (rem y borderlessTileSize)
+                          tile (getFromXY imageMap mapSize tx ty)
+                          ]
+                      (getValueAt tile (inc inTileX) (inc inTileY))))
+        ]
+    {:uid 0, :size mapW :tileArray tileArray}))
 
 
 (defn printTile
   [tileMap]
-  (doseq [[idx el] (map list (range 100) (get tileMap :tileArray))]
-    (if (= 0 (mod (+ idx 1) 10))
+  (doseq [[idx el] (map list (range (* (:size tileMap) (:size tileMap))) (get tileMap :tileArray))]
+    (if (= 0 (mod (+ idx 1) (:size tileMap)))
       (println el)
-      (print el)
-      )
-    )
+      (print el))))
+
+(defn buildFullTile
+  [inputLines tileSize]
+  (let [tilesArray (parseInput inputLines tileSize)
+        imageMap (buildMap tilesArray)
+        ]
+    (trimBorders imageMap)))
+
+
+(def MP [[0 0]
+         [1 1] [0 1] [-1 1] [-6 1] [-7 1] [-12 1] [-13 1] [-18 1]
+         [-2 2] [-5 2] [-8 2] [-11 2] [-14 2] [-17 2]])
+
+
+(defn patternFoundHere
+  [imageTile monsterPattern x y]
+
+  (allTrue (for [mIdx monsterPattern]
+             (let [patternX (+ x (first mIdx))
+                   patternY (+ y (last mIdx))]
+               (if (and (>= patternX 0)
+                        (< patternX (:size imageTile))
+                        (>= patternY 0)
+                        (< patternY (:size imageTile))
+                        (= \# (getValueAt imageTile patternX patternY)))
+                 true
+                 false)))))
+
+
+(defn addMonster
+  [imageTile monsterPattern mx my]
+
+  (for [y (range (:size imageTile))
+        x (range (:size imageTile))]
+    (loop [[index & tail] monsterPattern]
+      (if (not (some? index))
+        (getValueAt imageTile x y)
+        (if (and (= x (+ mx (first index)))
+                 (= y (+ my (last index))))
+          \O
+          (recur tail))))))
+
+(defn addAllMonsters
+  [imageTile monsterPattern positions]
+  (println "monster positions" positions)
+  (loop [tile imageTile
+         [pos & tail] positions
+         ]
+    (if (not (some? pos))
+      tile
+      (recur (into tile {:tileArray (addMonster tile monsterPattern (first pos) (last pos))}) tail))))
+
+(defn findMonsterLocations
+  [imageTile monsterPattern]
+  (filter some? (for [y (range (:size imageTile))
+                      x (range (:size imageTile))]
+                  (if (= \# (getValueAt imageTile x y))
+                    (if (true? (patternFoundHere imageTile monsterPattern x y))
+                      [x y]
+                      nil)
+                    nil))))
+
+
+(defn findMonster
+  [imageTile monsterPattern]
+
+  (let [trPair (for [tr [identity flipX flipY] rot [identity rotateR1 rotateR2 rotateR3]] [tr rot])]
+    (loop [currentTrList trPair]
+      (if (empty? currentTrList)
+        nil
+        (let [ [[trans rotation] & tail] currentTrList
+              searchTile (rotation (trans imageTile))
+              monsterLocations (findMonsterLocations searchTile monsterPattern)]
+          (if (not (empty? monsterLocations))
+            (addAllMonsters searchTile monsterPattern monsterLocations)
+            (recur tail)))))))
+
+(defn countWaters
+  [tileWithMonsters]
+  (count (filter #(= % \#) (:tileArray tileWithMonsters)))
   )
 
 
@@ -167,13 +316,15 @@
   "Day 20: Jurassic Jigsaw"
   [& args]
 
-  (println "part 1"
+  ;(println "part 1" (multiplyCorners (u/readLines inputFile) 10))
+
+  (println "part 2")
+  (println
     (let [lines (u/readLines inputFile)
-          tilesArray (parseInput lines)
-          corners (findCorners tilesArray)
+          imageTile (buildFullTile lines 10)
+          tileWithMonsters (findMonster imageTile MP)
           ]
-      (reduce #(* %1 (:uid %2)) 1 corners)))
-
-
-
-  )
+      (printTile imageTile)
+      (println "--------")
+      (printTile tileWithMonsters)
+      (countWaters tileWithMonsters))))
--- aoc-202-clojure/src/aoc/day20_test.clj
+++ aoc-202-clojure/src/aoc/day20_test.clj
@@ -11,14 +11,12 @@
       (is (= (seq "369258147") (:tileArray (rotateL inputTile))))
       (is (= (seq "321654987") (:tileArray (flipX inputTile))))
       (is (= (seq "789456123") (:tileArray (flipY inputTile))))
-      (is (= (seq "123456789") (:tileArray (rotateR (rotateR3 inputTile)))))
-      )))
+      (is (= (seq "123456789") (:tileArray (rotateR (rotateR3 inputTile))))))))
 
 (deftest testAllTrue
   (testing "allTrue function"
     (is (true? (allTrue '(true true true true))))
-    (is (= false (allTrue '(true false true true))))
-    ))
+    (is (= false (allTrue '(true false true true))))))
 
 
 (deftest matchTile
@@ -31,10 +29,62 @@
       (is (some? (itMatches tileOne (rotateL (flipX tileTwo)) "S")))
       (is (some? (itMatches tileOne (rotateL (rotateL (flipX tileTwo))) "S")))
       (is (some? (itMatches tileOne (rotateL (rotateL (flipY tileTwo))) "S")))
-      (is (=  nil (itMatches tileOne tileTwo "E")))
-      (is (=  nil (itMatches tileOne tileTwo "N")))
-      (is (=  nil (itMatches tileOne tileTwo "W")))
-      )))
+      (is (= nil (itMatches tileOne tileTwo "E")))
+      (is (= nil (itMatches tileOne tileTwo "N")))
+      (is (= nil (itMatches tileOne tileTwo "W"))))))
+
+(deftest testTrimBorders
+  (testing "trim borders"
+    (is (= (seq "2367230167760112")
+           (:tileArray (trimBorders [
+                                     {:uid 1, :size 4, :tileArray (seq "1234123412341234")}
+                                     {:uid 2, :size 4, :tileArray (seq "4567567890123456")}
+                                     {:uid 3, :size 4, :tileArray (seq "1234567890123456")}
+                                     {:uid 4, :size 4, :tileArray (seq "9999876831246315")}
+                                     ]))))))
+
+(deftest findMonsterTest
+  (testing "find monster pattern"
+    (let [mp [[0 0] [1 1] [0 1] [-1 1] [-3 1]]
+          inputTileArray (seq "#....#.#...####...#.#..#.#..###.#.###...#.##..#...##.#..#..##...##...#.#####...##....#......#..####.")
+          inputTile {:uid 0, :size 10, :tileArray inputTileArray}
+          ]
+      (is (= "#....#.#...####...#.#..#.O..###.O.OOO...#.##..#...##.#..#..##...O#...#.O#OOO...##....#......#..####." (str/join (:tileArray (findMonster inputTile mp))))))))
+
+
+(def INPUT_TILES
+  (str/split
+    "Tile 2311:\n..##.#..#.\n##..#.....\n#...##..#.\n####.#...#\n##.##.###.\n##...#.###\n.#.#.#..##\n..#....#..\n###...#.#.\n..###..###\n\nTile 1951:\n#.##...##.\n#.####...#\n.....#..##\n#...######\n.##.#....#\n.###.#####\n###.##.##.\n.###....#.\n..#.#..#.#\n#...##.#..\n\nTile 1171:\n####...##.\n#..##.#..#\n##.#..#.#.\n.###.####.\n..###.####\n.##....##.\n.#...####.\n#.##.####.\n####..#...\n.....##...\n\nTile 1427:\n###.##.#..\n.#..#.##..\n.#.##.#..#\n#.#.#.##.#\n....#...##\n...##..##.\n...#.#####\n.#.####.#.\n..#..###.#\n..##.#..#.\n\nTile 1489:\n##.#.#....\n..##...#..\n.##..##...\n..#...#...\n#####...#.\n#..#.#.#.#\n...#.#.#..\n##.#...##.\n..##.##.##\n###.##.#..\n\nTile 2473:\n#....####.\n#..#.##...\n#.##..#...\n######.#.#\n.#...#.#.#\n.#########\n.###.#..#.\n########.#\n##...##.#.\n..###.#.#.\n\nTile 2971:\n..#.#....#\n#...###...\n#.#.###...\n##.##..#..\n.#####..##\n.#..####.#\n#..#.#..#.\n..####.###\n..#.#.###.\n...#.#.#.#\n\nTile 2729:\n...#.#.#.#\n####.#....\n..#.#.....\n....#..#.#\n.##..##.#.\n.#.####...\n####.#.#..\n##.####...\n##..#.##..\n#.##...##.\n\nTile 3079:\n#.#.#####.\n.#..######\n..#.......\n######....\n####.#..#.\n.#...#.##.\n#.#####.##\n..#.###...\n..#.......\n..#.###..."
+    #"\n"))
+
+
+(deftest testCorners
+  (testing "find the corners"
+    (is (= 20899048083289 (multiplyCorners INPUT_TILES 10)))))
+
+
+(def MONSTER_IMG ".####...#####..#...###..#####..#..#.#.####..#.#..#.#...#.###...#.##.O#..#.O.##.OO#.#.OO.##.OOO##..#O.#O#.O##O..O.#O##.##...#.#..##.##...#..#..###.##.#..#.#..#..##.#.#...###.##.....#...###.#...#.####.#.#....##.#..#.#.##...#..#....#..#...####..#.##...###..#.#####..#....#.##.#.#####....#.....##.##.###.....#.##..#.#...#...###..####....##..#.##...#.##.#.#.###...##.###.#..####...##..#...#.###...#.##...#.##O###..O##.#OO.###OO##..OOO##...O#.O..O..O.#O##O##.####.#..##.########..#..##.#.#####..#.#...##..#....#....##..#.#########..###...#.....#..##...###.###..###....##.#...##.##.#")
+(def NO_MONSTER_IMG (str/replace MONSTER_IMG #"O" "#"))
+
+(deftest testLocations
+  (testing "test monster locations"
+    (is (true? (patternFoundHere {:uid 1, :size 24, :tileArray (seq NO_MONSTER_IMG)} MP 20 2)  ))
+    (is (true? (patternFoundHere {:uid 1, :size 24, :tileArray (seq NO_MONSTER_IMG)} MP 19 16)  ))
+    (is (= [[20 2] [19 16]] (findMonsterLocations {:uid 1, :size 24, :tileArray (seq NO_MONSTER_IMG)} MP)))
+    ))
+
+(deftest testMonsterImg_noTr
+  (testing "build image with monsters from direct input"
+    (is (= MONSTER_IMG (str/join (:tileArray (findMonster {:uid 1, :size 24, :tileArray (seq NO_MONSTER_IMG)} MP)))))))
+
+
+(deftest testCountWater
+  (testing "count water tiles"
+    (is (= 273 (countWaters (findMonster (buildFullTile INPUT_TILES 10) MP))))))
+
+(deftest testMonsterImg
+  (testing "build image with monsters"
+    (is (= MONSTER_IMG (str/join (:tileArray (findMonster (buildFullTile INPUT_TILES 10) MP)))))))
 
 
 (run-tests `aoc.day20_test)
