44ee13d6b4d2c576be53938e38527ec46cabb243
mihai <mihai.costin@gmail.com>
2021-12-07 15:46:25 +0200

    day7 part 2

--- aoc-2021-python/day7.py
+++ aoc-2021-python/day7.py
@@ -9,11 +9,26 @@ def to_numbers(file_name):
 
 input = to_numbers("day7.txt")
 #input = [16,1,2,0,4,2,7,1,2,14]
-numbers = np.array(input)
 
+# part 1
+numbers = np.array(input)
 mean = np.median(numbers)
 sum = 0
 for nr in input:
     sum += abs(nr - mean)
 
-print(sum)
+print("part 1", sum)
+
+
+# part 2
+
+min_sum = -1
+for m in range(min(numbers), max(numbers)):
+    sum = 0
+    for nr in input:
+        dif = abs(nr - m)
+        sum += (dif * (dif + 1)) / 2
+    if (min_sum == -1) | (sum < min_sum):
+        min_sum = sum
+
+print("part 2", min_sum)
