dbab9c1f817923db1e66a35fb91b2bf9f6ca2bad
mihai <mihai.costin@gmail.com>
2021-12-17 20:31:36 +0200

    day 14 - part 2

--- aoc-2021-python/day14.py
+++ aoc-2021-python/day14.py
@@ -16,36 +16,51 @@ def parse_input(file_name):
         return initial_value, transformations
 
 
-def generate_next(pair, rules):
+def generate_pair(pair, rules):
     for rule in rules:
         if rule[0] == pair:
-            return rule[1]
-    return ""
+            return [pair[0] + rule[1], rule[1] + pair[1]]
+    return [pair]
 
 
-def generate(polymer, rules):
-    new_polymer = []
-    for idx, element in enumerate(polymer):
-        if idx < len(polymer) - 1:
-            new_polymer.append(element + generate_next(element + polymer[idx + 1], rules))
-        else:
-            new_polymer.append(element)
-    return new_polymer
+# def generate(polymer, rules):
+#     new_polymer = []
+#     for idx, element in enumerate(polymer):
+#         if idx < len(polymer) - 1:
+#             new_polymer.append(element + generate_next(element + polymer[idx + 1], rules))
+#         else:
+#             new_polymer.append(element)
+#     return new_polymer
+#
 
+def generate(poly_pairs, rules):
+    new_poly_pairs = defaultdict(int)
+    for pair in poly_pairs:
+        current_count = poly_pairs[pair]
+        res = generate_pair(pair, rules)
+        for r in res:
+            new_poly_pairs[r] += current_count
+    return new_poly_pairs
 
 input = parse_input("day14.txt")
 print(input)
-
-polymer = input[0]
+polymer = input[0] + "#"
 rules = input[1]
 
-for i in range(0, 10):
-    polymer = generate(polymer, rules)
-    polymer = ''.join(polymer)
+poly_pairs = defaultdict(int)
+for idx, element in enumerate(polymer):
+    if idx < len(polymer) - 1:
+        poly_pairs[element+polymer[idx + 1]] += 1
+
+print(poly_pairs)
+
+for i in range(0, 40):
+    poly_pairs = generate(poly_pairs, rules)
+    print(poly_pairs)
 
 ch_count = defaultdict(int)
-for ch in polymer:
-    ch_count[ch] += 1
+for p in poly_pairs:
+    ch_count[p[0]] += poly_pairs[p]
 
 print(ch_count)
 print(max(ch_count.values()) - min(ch_count.values()))
