c06f5d6ba407ca95452750fcea9bf330b8ef61af
mihai <mihai.costin@gmail.com>
2022-12-16 22:34:00 +0200

    day 15 - p2

--- aoc-2022-js/src/day15.js
+++ aoc-2022-js/src/day15.js
@@ -51,34 +51,70 @@ parseInput(inFile)
             return {sx, sy, bx, by}
         })
     })
-    .then(points => points.map(p => {
-        const targetY = 2000000;
+    .then(points => processLine(points, 2_000_000))
+    .then(res => {
+        let sum = res.reduce((prev, current) => {
+            return prev + Math.abs(current.end - current.start) + 1;
+        }, 0);
+        console.log("part1", sum - 1); //account for the beacon already present on the line.
+    });
+
+
+//part 2
+parseInput(inFile)
+    .then(lines => {
+        return lines.map(line => {
+            let pattern = /(-?\d+)/g;
+            let [sx, sy, bx, by] = line.match(pattern).map(el => parseInt(el));
+            return {sx, sy, bx, by}
+        })
+    })
+    .then(points => {
+        let allLines = [];
+        for (let y = 0; y <= 4_000_000; y++) {
+            let intervals = processLine(points, y);
+            if (intervals.length === 1) {
+                let it = intervals[0];
+                if (it.start > 0 || it.end < 4_000_000) {
+                    console.log("possible", y, it.start, it.end);
+                }
+            }
+            if (intervals.length === 2) {
+                console.log("2!", y, intervals);
+            }
+
+            allLines.push(intervals);
+        }
+        return allLines;
+    })
+    .then(res => {
+        //console.log(res);
+        console.log(3385922 * 4000000 + 2671045);
+    });
+
+const processLine = (points, targetY) => {
+    let intervals = points.map(p => {
         let maxDistance = range(p.sx, p.sy, p.bx, p.by);
         let yDistanceToTarget = Math.abs(targetY - p.sy);
-        if (yDistanceToTarget <= maxDistance ) {
+        if (yDistanceToTarget <= maxDistance) {
             let xMax = Math.abs(maxDistance - yDistanceToTarget);
             let start = p.sx - xMax;
             let end = p.sx + xMax;
             return {start, end}
         }
-    }).filter(el => el !== undefined))
-    .then(intervals => {
-        let oldLen = intervals.length;
-        let collapsed = true;
-        while (collapsed) {
-            intervals = collapse(intervals);
-            collapsed = intervals.length !== oldLen;
-            oldLen = intervals.length;
-        }
-        return intervals;
-    })
-    .then(res => {
-        let sum = res.reduce((prev, current) => {
-            return prev + Math.abs(current.end - current.start) + 1;
-        }, 0);
-        console.log("sum", sum - 1); //account for the beacon already present on the line.
-    });
+    }).filter(el => el !== undefined);
+    intervals = collapseAll(intervals);
+    return intervals;
+}
 
-
-
+const collapseAll = (intervals) => {
+    let oldLen = intervals.length;
+    let collapsed = true;
+    while (collapsed) {
+        intervals = collapse(intervals);
+        collapsed = intervals.length !== oldLen;
+        oldLen = intervals.length;
+    }
+    return intervals;
+}
 
